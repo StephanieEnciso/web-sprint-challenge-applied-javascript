4dc0296169543cb850051f4285bf1cd8efd66465
user.email <stephanie.enciso13@gmail.com>
2020-12-04 12:32:35 -0600

    Working on the Cards.js

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,45 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios
+    .get('https://lambda-times-api.herokuapp.com/articles')
+    .then((res) => {
+        console.log(res);
+        const artInfo = res.data.articles
+        const newArtCard = cardMaker(artInfo);
+    })
+    .catch((err) => {
+        debugger;
+    });
+
+function cardMaker(article){
+    article.forEach((obj) => {
+        const cardDiv = document.createElement('div');
+        const headlineDiv = document.createElement('div');
+        const authorDiv = document.createElement('div');
+        const imgDiv = document.createElement('div');
+        const img = document.createElement('img');
+        const nameSpan = document.createElement('span');
+
+        cardDiv.classList.add('card');
+        headlineDiv.classList.add('headline');
+        authorDiv.classList.add('author');
+        imgDiv.classList.add('img-container');
+    
+        img.src = obj['autorPhoto'];
+        headlineDiv.textContent = obj['headline'];
+        nameSpan.textContent = 'By' obj['autorName'];
+
+        cardDiv.appendChild(headlineDiv);
+        cardDiv.appendChild(authorDiv);
+        authorDiv.appendChild(imgDiv);
+        authorDiv.appendChild(nameSpan);
+        imgDiv.appendChild(img);
+
+        cardDiv.addEventListener('click', function(event){
+            console.log(obj['headline']); 
+        });
+    });
+    return cardDiv;
+}
